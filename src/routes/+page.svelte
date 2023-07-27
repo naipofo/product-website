024373fda0cb7a8190b016c28fbb95f808a05404
naipofo <50967056+naipofo@users.noreply.github.com>
2023-07-27 17:26:00 +0200

    nicely animated header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<h1>the Product✨</h1>
+<script>
+	import AnimatedHeader from './AnimatedHeader.svelte';
+
+	let loaded = false;
+</script>
+
+<button
+	on:click={() => {
+		loaded = true;
+	}}>loaded</button
+>
+
+<AnimatedHeader title="Product" visible={loaded} />
---
+++ src/routes/AnimatedHeader.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
+	export let title = 'Title';
+	export let visible = true;
+</script>
+
+<section>
+	{#if visible}
+		<h1>
+			{#each title as l, i}
+				<span in:fade|global={{ delay: 600 + i * 100, duration: 700 }}>{l}</span>
+			{/each}
+		</h1>
+		<a href="./store" in:fade|global={{ delay: title.length * 100 + 1200 }}>order now!</a>
+	{/if}
+</section>
+
+<svelte:head>
+	<link
+		href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<style>
+	section {
+		font-family: 'IBM Plex Mono', monospace;
+	}
+	h1 {
+		font-weight: 700;
+		font-size: max(8vw, 3rem);
+		margin-bottom: 1rem;
+	}
+	a {
+		font-weight: 700;
+		font-size: 2.5rem;
+	}
+	section {
+		padding: 5rem 5rem 5rem 7rem;
+	}
+</style>
